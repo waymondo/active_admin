d29c4af3600edbffd751e26b57dfaa2409e04199
justin talbott <justin@waymondo.com>
2012-10-14 18:09:04 -0400

    timepicker strftime string formatting for proper initialization

--- lib/active_admin/inputs/timepicker_input.rb
+++ lib/active_admin/inputs/timepicker_input.rb
@@ -4,7 +4,11 @@ module ActiveAdmin
       def input_html_options
         options = super
         options[:class] = [options[:class], "timepicker"].compact.join(' ')
+        options[:value] = formatted_time
         options
+      end
+      def formatted_time
+        @object.send(method).blank? ? "" : @object.send(method).strftime("%I:%M%P")
       end
     end
   end
